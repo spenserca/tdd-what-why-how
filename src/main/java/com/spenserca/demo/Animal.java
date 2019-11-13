c948bf8d35b59bae1683d2fadfdfb0f6f1699736
Spenser Andreassen <spenserca@gmail.com>
2019-11-12 21:22:50 -0600

    feat: stub out some basic classes

---
+++ src/main/java/com/spenserca/demo/Animal.java
@@ -0,0 +1,17 @@
+package com.spenserca.demo;
+
+public class Animal {
+    private String name;
+    private String description;
+    private String funFact;
+
+    public Animal(
+            String name,
+            String description,
+            String funFact
+    ) {
+        this.name = name;
+        this.description = description;
+        this.funFact = funFact;
+    }
+}
---
+++ src/main/java/com/spenserca/demo/SomethingToMock.java
@@ -0,0 +1,7 @@
+package com.spenserca.demo;
+
+public class SomethingToMock {
+    public void doSomething(Object o) {
+        System.out.println("o.toString: " + o.toString());
+    }
+}
